e5b9d549f979c959752bfd63744fc09828ba9525
cesarRz <cesarrz2000@gmail.com>
2023-03-03 16:29:26 -0600

    Initial Commit

---
+++ Proyecto_MT/Proyecto_MTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Proyecto_MTApp.swift
+//  Proyecto_MT
+//
+//  Created by Cesar Roman Zuniga on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Proyecto_MTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
